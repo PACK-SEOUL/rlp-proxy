5f2640625a0a8d8186da83ea630289ce808e6c10
kjsmania <kjsmania@gmail.com>
2024-05-29 17:07:13 +0900

    fix import

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 require('dotenv').config();
 import express, { Response } from 'express';
-import { getMetadata } from './lib';
-
 import cron from 'node-cron';
+import fetch from 'node-fetch'; // Import node-fetch
+import { getMetadata } from './lib';
 // import { checkForCache, createCache } from './lib/cache';
 import { APIOutput } from './types';
 
