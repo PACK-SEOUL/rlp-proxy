c78d5ddbfad94b160b377ce59c9c5681256b8cce
kjsmania <kjsmania@gmail.com>
2024-05-29 17:49:45 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ require('dotenv').config();
 import express, { Response } from 'express';
 import cron from 'node-cron';
 import fetch from "node-fetch";
-import { getMetadata } from './lib/index';
-import { APIOutput } from './types/index';
+import { getMetadata } from './lib/index.js';
+import { APIOutput } from './types/index.js';
 
 const app = express();
 
